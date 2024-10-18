dad0ef51ad71dbebcae7451e51622cf7b62c5aca
TheGilfoyle <budabelhassen@gmail.com>
2024-10-18 22:49:40 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,17 @@ public class Main {
     public static void main(String[] args) {
         Battle b = new Battle();
         Pokemon p1 = new Lunatone("Lunatone", 8);//полиморфизм
-        Flygon p2 = new Flygon("Flygon", 4);
-        /*Pokemon p3 = new Jolteon("Покемон 3", 1);
-        Pokemon p4 = new Trapinch("Покемон 4", 1);
-        Pokemon p5 = new Vibrava("Покемон 5", 1);
-        Pokemon p6 = new Flygon("Покемон 6", 1);
-         */
+        Pokemon p2 = new Flygon("Flygon", 4);
+        Pokemon p3 = new Jolteon("Jolteon", 7);
+        Pokemon p4 = new Trapinch("Trapinch", 6);
+        Pokemon p5 = new Vibrava("Vibrava", 5);
+        Pokemon p6 = new Flygon("Flygon", 7);
         b.addAlly(p1);
-        b.addFoe(p2);
-        /*
+        b.addAlly(p2);
         b.addAlly(p3);
         b.addFoe(p4);
         b.addFoe(p5);
         b.addFoe(p6);
-
-         */
-        Effect e = new Effect().condition(Status.BURN);
-        p1.addEffect(e);
         b.go();
     }
 }
